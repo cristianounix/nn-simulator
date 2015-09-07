91c823807ab2b4965e7a8107554bcd2dcd1be8dd
dextra <dextra@nbc-cps04.(none)>
2015-09-06 22:17:10 -0300

    Initial code

---
+++ src/main/java/br/fee/unicamp/App.java
@@ -0,0 +1,13 @@
+package br.fee.unicamp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
